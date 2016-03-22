b35fb4b14c64361d12d57e0147e40ec1349345e2
lacava <williamlacava@gmail.com>
2016-03-22 11:13:02 -0400

    added subtree mutation to EpiHC

--- DevelEP/EpiHC.cpp
+++ DevelEP/EpiHC.cpp
@@ -20,7 +20,7 @@ void EpiHC(ind& oldind,params& p,vector<Randclass>& r,Data& d,state& s,FitnessEs
 		//makenew(tmp_ind[0]);
 		//tmp_ind[0].clrPhen();
 		bool updated = false;
-		int numchanges = 0;
+		int min_change;
 		bool pass = true;
 		int outstart = 0;
 		int linestart = 0;
@@ -39,25 +39,59 @@ void EpiHC(ind& oldind,params& p,vector<Randclass>& r,Data& d,state& s,FitnessEs
 			tmp_ind.resize(0);
 			tmp_ind.push_back(oldind);
 			tmp_ind[0].clrPhen();
-			
-			for(unsigned int h = 0;h<tmp_ind[0].line.size();++h)
-			{
-				if(r[omp_get_thread_num()].rnd_flt(0,1)<=p.eHC_prob){
-					tmp_ind[0].line.at(h).on = !tmp_ind[0].line.at(h).on;
-					numchanges++;
-					if (numchanges==1){
-						linestart = h;
-						for (int x = 0; x<h;x++)
-							outstart+=(int)(tmp_ind[0].line.at(x).on);
+			min_change = tmp_ind[0].line.size()+1;
+
+			for(int h = tmp_ind[0].line.size()-1;h>=0;--h)
+			{				
+				if (r[omp_get_thread_num()].rnd_flt(0, 1) <= p.eHC_prob) {
+					int pt1 = h;
+					if (p.cross == 3) { // turn off a subtree
+						int sum_arity = tmp_ind[0].line[pt1].arity_float;
+						tmp_ind[0].line[pt1].on = !tmp_ind[0].line[pt1].on;
+						while (sum_arity > 0 && pt1 > 0)
+						{							
+							--pt1;
+							--sum_arity;
+							sum_arity += tmp_ind[0].line[pt1].arity_float;
+							tmp_ind[0].line[pt1].on = !tmp_ind[0].line[pt1].on;
+						}	
+					}
+					else 
+						tmp_ind[0].line.at(h).on = !tmp_ind[0].line.at(h).on;
+					
+					if (pt1 < min_change){	// determine active nodes occuring before change point		
+						min_change = pt1;
+						
 					}
 				}
+				
 			}	
-			if (numchanges==0){
+
+			if (min_change==tmp_ind[0].line.size()+1){
 				int tmp = r[omp_get_thread_num()].rnd_int(0,tmp_ind[0].line.size()-1);
-				tmp_ind[0].line.at(tmp).on = !tmp_ind[0].line.at(tmp).on;
-				linestart=tmp;
+				int pt1 = tmp;
+				if (p.cross == 3) { // turn off a subtree
+					int sum_arity = tmp_ind[0].line[pt1].arity_float;
+					tmp_ind[0].line[pt1].on = !tmp_ind[0].line[pt1].on;
+					while (sum_arity > 0 && pt1 > 0)
+					{
+						--pt1;
+						--sum_arity;
+						sum_arity += tmp_ind[0].line[pt1].arity_float;
+						tmp_ind[0].line[pt1].on = !tmp_ind[0].line[pt1].on;
+					}
+				}
+				else
+					tmp_ind[0].line.at(tmp).on = !tmp_ind[0].line.at(tmp).on;
+
+				linestart=pt1;
 				for (int x = 0; x<tmp;x++)
 					outstart+=(int)(tmp_ind[0].line.at(x).on);
+			}
+			else {
+				linestart = min_change;
+				for (int x = 0; x < linestart; ++x)
+					outstart += (int)(tmp_ind[0].line[x].on);
 			}
 
 			//Gen2Phen(tmp_ind,p);
@@ -96,7 +130,7 @@ void EpiHC(ind& oldind,params& p,vector<Randclass>& r,Data& d,state& s,FitnessEs
 			else 
 				pass = 1;
 
-			numchanges=0;
+			//numchanges=0;
 			outstart=0;
 		}
 		//tmp_ind.clear();
